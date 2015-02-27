eac0cdcdfdd47746dcb12c04fcf8c8f16dd90322
Phildo <phildo211@gmail.com>
2015-02-26 18:43:02 -0600

    timer

--- src/scenes/gameplayScene.js
+++ src/scenes/gameplayScene.js
@@ -15,6 +15,9 @@ var GamePlayScene = function(game, stage)
     self.text = str;
     self.progress = 0;
 
+    //partitioned strings 
+    self.fullWidthLines = []; 
+
     self.key = function(k)
     {
       if(k == self.text.substring(self.progress,self.progress+1).toLowerCase())
@@ -45,8 +48,43 @@ var GamePlayScene = function(game, stage)
     }
   }
 
+  var Timer = function()
+  {
+    var self = this;
+
+    self.t = 0;
+    self.total = 1000;
+
+    self.x = 200;
+    self.y = 200;
+    self.w = 200;
+    self.h = 200;
+
+    self.draw = function(canv)
+    {
+      canv.context.strokeStyle = "#00FFFF";
+      canv.context.lineWidth = 5;
+      canv.context.beginPath();
+      canv.context.arc(
+      self.w/2,
+      self.h/2,
+      (self.w/2)-5,
+      3*Math.PI/2,
+      (3*(Math.PI/2)+(self.t/self.total)*(2*Math.PI))%(2*Math.PI)+0.01,
+      true);
+      canv.context.stroke();
+    }
+
+    self.tick = function()
+    {
+      if(self.t < self.total) self.t++;
+      else self.t = 0;
+    }
+  }
+
   var mono;
   var mono_disp;
+  var timer;
 
   self.ready = function()
   {
@@ -59,9 +97,12 @@ var GamePlayScene = function(game, stage)
 
     mono = new Monologue(tgen.getMonologue());
     mono_disp = new MonologueDisplay(mono);
+    timer = new Timer();
 
     keyer.register(mono);
     drawer.register(mono_disp);
+    drawer.register(timer);
+    ticker.register(timer);
   };
 
   self.tick = function()
