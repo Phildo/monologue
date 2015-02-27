ac426d60caf1ef22cd5c75dac952449e39a9e6e8
Phildo <phildo211@gmail.com>
2015-02-26 18:23:31 -0600

    windows permissions

--- src/scenes/textgen.js
+++ src/scenes/textgen.js
@@ -11,7 +11,11 @@ var TextGen = function()
   self.getMonologue = function()
   {
     //return "I love big bananas they are very ripe and tasty yumm yumm.";
-    return randomshit[Math.floor(Math.random()*randomshit.length)];
+    //return randomshit[Math.floor(Math.random()*randomshit.length)];
+    return "Ahaha! Now that I, the baddest and most rootin-tootin bandito William Todd Marsh, have unmounted and defeated the almighty Sheriff of Easton, I will gather my rowdy gang and we will ransack the general goods store and rob your bank for all its worth.";
   }
 }
 
+var a = new TextGen(); //
+
+document.write(a.getMonologue());
