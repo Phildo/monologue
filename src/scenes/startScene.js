3ed808c70f68f1773446af4e60007723d340e7c5
Phildo <phildo211@gmail.com>
2015-02-27 22:20:45 -0600

    intro scene fade out

--- src/scenes/startScene.js
+++ src/scenes/startScene.js
@@ -5,7 +5,7 @@ var StartScene = function(game, stage)
   var PlayButton = function()
   {
     var self = this;
-    var toggleTextColor = false;
+    var toggleTextColor = true;
     var tickCount = 0;
 
     var progressString = "play";
@@ -16,55 +16,81 @@ var StartScene = function(game, stage)
       //NOTE: canv.canvas.width/.height to access those
 
 
-
-      //back
+      //story box
       canv.context.fillStyle = "#000000";
-      canv.context.fillRect(140, 200, 620, 200); //x, y, width, eight
-
+      canv.context.fillRect(75, 50, 750, 450); //x, y, width, eight
       canv.context.fillStyle = "#FFFFFF";
-      canv.context.fillRect(150, 210, 600, 180); //x, y, width, eight
-
-          canv.context.fillStyle = "#999999";
-          canv.context.font = "144px vg_font";
-          canv.context.fillText("play", 154, 365);
-
-        if(toggleTextColor)
-        {
-          canv.context.fillStyle = "#FF0000";       
-        }
-        else
-        {
-          canv.context.fillStyle = "#999999";
-        }
-        canv.context.font = "144px vg_font";
-        canv.context.fillText(progressString.substring(0,progressInt+1), 154, 365);
-
-        canv.context.fillStyle = "#000000";
-        canv.context.font = "144px vg_font";
-        canv.context.fillText(progressString.substring(0,progressInt), 154, 365);
+      canv.context.fillRect(85, 60, 730, 430); //x, y, width, eight
 
 
-      if(progressInt <= progressString.length)
+      canv.context.fillStyle = "#000000";
+      canv.context.font = "24px vg_font";
+      canv.context.fillText(" You are the baddest villain", 100, 204);//str, x, y
+      canv.context.fillText("         in the west.", 100, 228);//str, x, y   //topkek -> #hackcity dawg
+
+
+      canv.context.fillStyle = "#000000";
+      canv.context.font = "24px vg_font";
+      canv.context.fillText(" Again, you have caught your", 100, 252+24);//str, x, y
+      canv.context.fillText("         archnemesis.", 100, 276+24);//str, x, y
+
+
+      canv.context.fillStyle = "#000000";
+      canv.context.font = "24px vg_font";
+      canv.context.fillText(" Deliver the final monologue", 100, 300+48);//str, x, y
+      canv.context.fillText("      before they escape!", 100, 324+48);//str, x, y
+
+
+      canv.context.fillStyle = "#FF1F0A";
+      canv.context.font = "24px vg_font";
+      canv.context.fillText("      Type to start...", 100, 348+96);//str, x, y
+
+
+
+
+      //textbox
+      canv.context.fillStyle = "#000000";
+      canv.context.fillRect(140, 450, 620, 200); //x, y, width, eight
+      canv.context.fillStyle = "#FFFFFF";
+      canv.context.fillRect(150, 460, 600, 180); //x, y, width, eight
+
+
+
+
+      //boxbehindtopreventborderconflict
+      canv.context.fillStyle = "#FFFFFF";
+      canv.context.fillRect(75, 65, 750, 96); //x, y, width, eight
+      //title text
+      canv.context.fillStyle = "#000000";
+      canv.context.font = "96px vg_font";
+      canv.context.fillText("Monologue", 10, 154);//str, x, y
+
+
+
+
+      //play text
+      canv.context.font = "144px vg_font";
+      canv.context.fillStyle = "#999999";
+      canv.context.fillText("play", 154, 615);
+
+      if(toggleTextColor)
       {
-        for(var i = 0; i < progressString.length; i++)
-        {
-          if(i == progressInt)
-          {
-
-            if(toggleTextColor)
-            {
-              canv.context.fillStyle = "#FF0000";
-            }
-            else
-            {
-              canv.context.fillStyle = "#999999"
-            }
-
-          }
-        }
+        canv.context.fillStyle = "#FF0000";       
       }
+      else
+      {
+        canv.context.fillStyle = "#999999";
+      }
+      canv.context.fillText(progressString.substring(0,progressInt+1), 154, 615);
+
+      canv.context.fillStyle = "#000000";
+      canv.context.fillText(progressString.substring(0,progressInt), 154, 615);
 
     }
+
+
+
+
     //by saying self = this, we can explicitly make sure that self refers to this object
     self.key = function(k)
     {
@@ -72,6 +98,8 @@ var StartScene = function(game, stage)
       if(k == progressString[progressInt])
       {
         progressInt++;
+        tickCount = 0;
+        toggleTextColor = true;
       }
       else
       {
@@ -91,7 +119,8 @@ var StartScene = function(game, stage)
       }
       if(progressInt >= 4)
       {
-        game.nextScene();
+        shouldTransition = true;
+        // game.nextScene();
       }
     }
   }
@@ -109,17 +138,34 @@ var StartScene = function(game, stage)
     self.ticker.register(self.playButton);
   };
 
+
+  var shouldTransition = false;
+  var basedAlphaTrainsition = 0.0;
+
   self.tick = function()
   {
 
-//game.nextScene();
     self.keyer.flush();
     self.ticker.flush();
+
+    if(basedAlphaTrainsition >= 1.1)
+    {
+      game.nextScene();
+    }
+
   };
 
   self.draw = function()
   {
     self.drawer.flush();
+
+    if(shouldTransition)
+    {
+      var canv = stage.drawCanv;
+      canv.context.fillStyle = "rgba(0,0,0,"+basedAlphaTrainsition+")";
+      canv.context.fillRect(0,0,canv.canvas.width,canv.canvas.height);
+      basedAlphaTrainsition += .01;
+    }
   };
 
   self.cleanup = function()
